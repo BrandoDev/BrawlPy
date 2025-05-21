e17e33d6c59ea3e8ea9fdaa0239becc4e4d3b629
BrandoDev <53405682+BrandoDev@users.noreply.github.com>
2025-05-21 16:52:49 +0000

    allowed_chars fix

--- brawlpy/utils.py
+++ brawlpy/utils.py
@@ -2,13 +2,13 @@ from .errors import *
 
 def checkTag(tag : str):
     tag = tag.strip("#").upper()
-    allowed_chars = "0289PYLQGRJCUV"
+    allowed_chars = "0289PYLQGRJCUVO"
 
     if len(tag) < 3:
         raise TagNotFoundError(404, reason="Tag can't be less then 3 characters!")
     invalid = [i for i in tag if i not in allowed_chars]
     if invalid:
-        raise TagNotFoundError(404, reason="An Invalid character has been passed!",invalid_characters=invalid)
+        raise TagNotFoundError(404, reason=f"An Invalid character has been passed! {invalid}")
 
     if not tag.startswith("%23"):
         tag = "%23" + tag
